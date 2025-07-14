db2531e4e31b826884e02037c5d9af7d739d3b0d
Michael Henrich <michael.henrich@dedalus.com>
2025-07-14 10:00:10 +0200

    HCONN-22271 fail early if no valid token found

--- get-token.sh
+++ get-token.sh
@@ -10,13 +10,25 @@ TOKEN_SERVER=$1
 USERNAME=$2
 PASSWORD=$3
 
-export BEARER_TOKEN=$(curl \
+if RESPONSE=$(curl \
   --disable \
-  --silent \
   --insecure \
+  --silent \
+  --fail \
   --header "Content-Type: application/x-www-form-urlencoded" \
   --data "grant_type=password&client_id=orbis-u-webclient&orbis_facility_id=1&username=${USERNAME}&password=${PASSWORD}&=" \
-  "https://${TOKEN_SERVER}/auth/realms/orbis/protocol/openid-connect/token" | \
-    sed \
-      --regexp-extended \
-      --expression 's/\{"access_token":"([^"]*).*/\1/')
+  "https://${TOKEN_SERVER}/auth/realms/orbis/protocol/openid-connect/token"); then
+
+  BEARER_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
+
+  if [ -z "$BEARER_TOKEN" ] || [ "$BEARER_TOKEN" == "null" ]; then
+    echo "Token is missing or malformed in the JSON response"
+    exit 1
+  fi
+
+  export BEARER_TOKEN
+  echo "Valid token retrieved. Starting tests..."
+else
+  echo "Failed to retrieve token. Please check the host address, your username and password"
+  exit 1
+fi
